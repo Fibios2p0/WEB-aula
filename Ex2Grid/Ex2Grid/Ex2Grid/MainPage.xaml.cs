4d817d4c80a7e049a7bf53b2cbc4a41c471a9917
BrunoFacul <bruno.o@aluno.ifsp.edu.br>
2019-08-16 21:12:50 -0300

    Exercícios Aula3 Dispositivos Móveis Layouts

---
+++ Ex2Grid/Ex2Grid/Ex2Grid/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ex2Grid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            listViewVeiculos.ItemsSource = new string []
+            {
+                "Maverick V6",
+                "Monza",
+                "Ford Galaxy"
+            };
+        }
+    }
+}
---
+++ ListView/ListView/ListView/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var alunos = new string[]
+            {
+                "Amanda Lorenzetti",
+                "Mario Hikari",
+                "Willian Bastos"
+            };
+            listViewAlunos.ItemsSource = alunos;
+        }
+    }
+}
