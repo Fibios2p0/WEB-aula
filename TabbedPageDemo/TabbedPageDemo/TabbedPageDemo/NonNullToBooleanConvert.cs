98305a590b0444af796ddce11386f19d4f04bb35
BrunoIFSP <bruno.o@aluno.ifsp.edu.br>
2019-08-30 21:40:08 -0300

    Layouts dois Projetos

---
+++ TabbedPageDemo/TabbedPageDemo/TabbedPageDemo/NonNullToBooleanConvert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TabbedPageDemo
+{
+     public  class NonNullToBooleanConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+                return !string.IsNullOrEmpty((string)value);
+            return value != null;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
